725e678ddcc44f4b065d97dc34c4ab98ecb8d9d0
Tom <tom.garne@uwe.ac.uk>
2022-07-22 09:31:27 +0100

    updated textures

--- Assets/Scripts/RotateModelMouse.cs
+++ Assets/Scripts/RotateModelMouse.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class RotateModelMouse : MonoBehaviour
 {	
 	public GameObject rotationObject;
-    public float transparencySensitivity = 5f;
-    public int transparencySmoothing = 20;
+    public float transparencySensitivity = 10f;
+    public int transparencySmoothing = 10;
+
+    private bool transparent = false;
+
+    public Material opaqueMaterial;
+    public Material transparentMaterial;
 
     public float speed = 0;
 
@@ -22,14 +26,13 @@ public class RotateModelMouse : MonoBehaviour
 
     private List<Vector3> previousAngles;
 
-
     private Quaternion current;
 
     public float playbackSpeed = 1/100;
     
     private float previousTime = 0;
 
-    private enum Mode {SLOW,SPEEDUP, NORMAL};
+    private enum Mode {SLOWDOWN, SLOW, SPEEDUP, NORMAL};
 
     private Mode currentMode = Mode.NORMAL;
 
@@ -51,7 +54,10 @@ public class RotateModelMouse : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
          Cursor.visible = false; 
 
-        nextModeChangeTime = Time.time + Random.Range(4,7);
+        nextModeChangeTime = Time.time + Random.Range(9,11);
+
+        //opaqueMaterial = rotationObject.GetComponent<Renderer>().material;
+
     }
 
     void Update(){
@@ -63,23 +69,25 @@ public class RotateModelMouse : MonoBehaviour
         if(currentMode == Mode.NORMAL){
     	    current = Quaternion.Euler(previousAngles[0].x,previousAngles[0].y,previousAngles[0].z);
             previousAngles.RemoveAt(0);
-        }else if(currentMode == Mode.SLOW){
+        }else if(currentMode == Mode.SLOWDOWN){
             if(previousTime > (Time.deltaTime * 2)){
                 current = Quaternion.Euler(previousAngles[0].x,previousAngles[0].y,previousAngles[0].z);
                 previousAngles.RemoveAt(0);
                 previousTime = 0;
             }
+        }else if(currentMode == Mode.SLOW){
+            current = Quaternion.Euler(previousAngles[0].x,previousAngles[0].y,previousAngles[0].z);
+            previousAngles.RemoveAt(0);
         }else if(currentMode == Mode.SPEEDUP){
-
             current = Quaternion.Euler(previousAngles[1].x,previousAngles[1].y,previousAngles[1].z);
             previousAngles.RemoveAt(0);
             previousAngles.RemoveAt(0);
-
         }
 
 
 
         transform.rotation = current;
+
 
         //store difference between current angle and last
     	float angle = Quaternion.Angle(previousAngle, current);
@@ -92,8 +100,20 @@ public class RotateModelMouse : MonoBehaviour
 		}
 	
         //set object transparency
-        rotationObject.GetComponent<Renderer>().material.color = new Color(1.0f,1.0f,1.0f,calculateTransparency() );
-      
+        float newTransparency = calculateTransparency();
+        if(newTransparency < 0.8f && !transparent){
+            rotationObject.GetComponent<Renderer>().material = transparentMaterial;
+            rotationObject.GetComponent<Renderer>().material.color = new Color(1.0f,1.0f,1.0f,newTransparency);
+            transparent = true;
+        }else if(newTransparency >= 0.8f && transparent){
+            rotationObject.GetComponent<Renderer>().material = opaqueMaterial;
+            transparent = false;
+        }
+              
+        //set scale
+        float newScale = 1 - (newTransparency/3);
+        transform.localScale = new Vector3(newScale,newScale,newScale);
+        
         //Quit if escape key pressed
         if(Input.GetKeyDown(KeyCode.Escape)){
 		    Application.Quit();
@@ -110,23 +130,28 @@ public class RotateModelMouse : MonoBehaviour
         if(currentMode == Mode.NORMAL){
             //check to see if want to slow down
             if(Time.time > nextModeChangeTime){
+                currentMode = Mode.SLOWDOWN;
+               
+                nextModeChangeTime = Time.time + Random.Range(9,11);
+            }
+        }else if(currentMode == Mode.SLOWDOWN){
+            if(Time.time > nextModeChangeTime){
                 currentMode = Mode.SLOW;
-                Debug.Log("change to slow mode");
-                nextModeChangeTime = Time.time + Random.Range(3,8);
+                nextModeChangeTime = Time.time + Random.Range(9,11);
             }
         }else if(currentMode == Mode.SLOW){
-            if(Time.time > nextModeChangeTime){
-                currentMode = Mode.SPEEDUP;
-            }
+             if(Time.time > nextModeChangeTime){
+                 currentMode = Mode.SPEEDUP;
+             }
         }else if(currentMode == Mode.SPEEDUP){
             if(previousAngles.Count <= 1){
                 currentMode = Mode.NORMAL;
                 previousAngles.Clear();
-                nextModeChangeTime = Time.time + Random.Range(4,7);
+                nextModeChangeTime = Time.time + Random.Range(9,11);
             }
         }
 
-
+       Debug.Log(newTransparency);
     }
 
     // MessageReceived implementation
@@ -153,14 +178,14 @@ public class RotateModelMouse : MonoBehaviour
 		for(int i = 0; i< previousQuaternionDifferences.Count; i++){
 			total += previousQuaternionDifferences[i];
 		}
+
 		float average = total/previousQuaternionDifferences.Count;
         float calTransparency = 1.0f-(average/transparencySensitivity);
 
         //clamp transparency between 0 and 1
-        calTransparency = Mathf.Clamp(calTransparency,0.01f,1f);
+        calTransparency = Mathf.Clamp(calTransparency,0f,255f);
 
         return calTransparency;
     }
-
 
 }
